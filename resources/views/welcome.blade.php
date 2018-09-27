3cfaad11a499b8cb68bf7c4c9a4c0f95d296f9fe
Jefta Chibiya <jeftachibiya@Jeftas-MacBook-Pro.local>
2018-09-27 15:28:07 +0100

    Hona Ltd initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+            <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 284.14 286">
+                <defs>
+                    <style>.cls-1{fill:#fbae17;stroke:#fbae17;stroke-miterlimit:10;stroke-width:1.55px;}.cls-2{fill:#fddb00;}</style>
+                </defs>
+                <title>Hona Ltd</title>
+                <path class="cls-1" d="M21,17" transform="translate(-9.93 -8)"/><path class="cls-1" d="M197,204" transform="translate(-9.93 -8)"/><ellipse class="cls-2" cx="142.07" cy="143" rx="142.07" ry="143"/>
+                <path d="M103.92,154.69v15.8H97.17V135.15h6.75v14.17H116.5V135.15h6.8v35.34h-6.8v-15.8Z" transform="translate(-9.93 -8)"/>
+                <path d="M155.07,156.94c0,8.54-4.76,14.32-12.74,14.32s-12.73-5.42-12.73-14.32c0-8.49,4.75-14.27,12.78-14.27S155.07,148.09,155.07,156.94Zm-18.57,0c0,6.49,2,9.36,5.83,9.36s5.83-2.92,5.83-9.36-2-9.31-5.78-9.31S136.5,150.54,136.5,156.94Z" transform="translate(-9.93 -8)"/>
+                <path d="M183.31,150.9v19.59h-6.55V151.92c0-3.37-1.28-4.34-3.32-4.34-2.36,0-4.1,1.58-5.68,4.09v18.82h-6.55V143.43h5.68l.51,3.53a10.09,10.09,0,0,1,8.23-4.29C180.49,142.67,183.31,145.74,183.31,150.9Z" transform="translate(-9.93 -8)"/>
+                <path d="M213,166.81l-1.39,4.35c-3-.26-4.91-1.33-6-3.94a9.22,9.22,0,0,1-8.08,4c-5.42,0-8.75-3.43-8.75-8.34,0-5.88,4.45-9,12.33-9h3.38v-1.54c0-3.53-1.59-4.7-5.07-4.7a24.15,24.15,0,0,0-7.16,1.43l-1.53-4.45a27.85,27.85,0,0,1,9.82-1.94c7.21,0,10.38,3.32,10.38,9.31v11.35C210.93,165.53,211.65,166.3,213,166.81Zm-8.49-3.63V157.7h-2.72c-4.24,0-6.34,1.54-6.34,4.71,0,2.61,1.33,4.09,3.84,4.09A5.78,5.78,0,0,0,204.49,163.18Z" transform="translate(-9.93 -8)"/>
+            </svg>                
+            </div>
+        </div>
+    </body>
+</html>
